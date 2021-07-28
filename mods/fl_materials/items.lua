70fafd44385e68badce2186cbb930017527231e3
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2021-07-28 15:21:41 -0300

    add coin gold

--- mods/fl_materials/items.lua
+++ mods/fl_materials/items.lua
@@ -8,7 +8,7 @@ minetest.register_craftitem("fl_materials:silver_coin", {
     inventory_image = "silver_coin.png"
 })
 
-minetest.register_craftitem("fl_materials:copper_coin", {
-    description = "Copper Coin",
-    inventory_image = "copper_coin.png"
+minetest.register_craftitem("fl_materials:gold_coin", {
+    description = "Gold Coin",
+    inventory_image = "gold_coin.png"
 })
