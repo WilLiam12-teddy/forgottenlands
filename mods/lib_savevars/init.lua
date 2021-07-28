117de5ce3f4b5c32c44eeeaf2dd6d3d755642e2b
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2021-07-27 21:46:55 -0300

    j

--- mods/lib_savevars/init.lua
+++
@@ -1,7 +0,0 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-
-dofile(modpath.."/api.lua")
-dofile(modpath.."/commands.lua")
-
-minetest.log('action',"["..modname:upper().."] Carregado!")
