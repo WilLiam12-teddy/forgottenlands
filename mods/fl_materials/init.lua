8b8fcabcc3bec61bda56a2763545ad595df8ee4e
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2021-07-28 00:42:47 -0300

    yeah, bug fixed

---
+++ mods/fl_materials/init.lua
@@ -0,0 +1,10 @@
+local modpath = minetest.get_modpath("fl_materials")
+
+custom_materials = {}
+
+dofile(modpath.."/nodes.lua")
+dofile(modpath.."/ores.lua")
+dofile(modpath.."/items.lua")
+
+-- Forgotten Lands Materials
+-- items, ores and nodes for FL server. 
---
+++ mods/fl_materials/items.lua
@@ -0,0 +1,14 @@
+minetest.register_craftitem("fl_materials:copper_coin", {
+    description = "Copper Coin",
+    inventory_image = "copper_coin.png"
+})
+
+minetest.register_craftitem("fl_materials:silver_coin", {
+    description = "Silver Coin",
+    inventory_image = "silver_coin.png"
+})
+
+minetest.register_craftitem("fl_materials:copper_coin", {
+    description = "Copper Coin",
+    inventory_image = "copper_coin.png"
+})
