56ba20014effb5dde343c8eb1500c96f8ba400e2
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2021-08-05 18:35:23 -0300

    update fl_materials ...

--- mods/fl_materials/items.lua
+++ mods/fl_materials/items.lua
@@ -1,6 +1,8 @@
 
 local tool_stats = {maxlevel=2, uses=10, times={[1]=1.50, [2]=1.20, [3]=0.80}}
 
+--- Itens
+
 minetest.register_craftitem("fl_materials:copper_coin", {
     description = "Copper Coin",
     inventory_image = "copper_coin.png"
@@ -16,8 +18,22 @@ minetest.register_craftitem("fl_materials:gold_coin", {
     inventory_image = "gold_coin.png"
 })
 
+minetest.register_craftitem("fl_materials:meteorite_fragment",{
+
+    description = "meteorite fragment",
+    inventory_image = "meteorite_fragment.png"
+})
+
+minetest.register_craftitem("fl_materials:meteorite_ingot",{
+
+    description = "meteorite ingot",
+    inventory_image = "meteorite_ingot.png"
+})
+
+--- Tools
+
 minetest.register_tool("fl_materials:meteorite_pick",{
-    description = "Meteorite Pickaxe",
+    description = "Meteorite pickaxe",
     inventory_image = "tool_meteoritepick.png",
     range = 10,
     tool_capabilities = {
@@ -31,7 +47,7 @@ minetest.register_tool("fl_materials:meteorite_pick",{
 })
 
 minetest.register_tool("fl_materials:meteorite_axe",{
-    description = "Meteorite Axe",
+    description = "Meteorite pickaxe",
     inventory_image = "tool_meteoriteaxe.png",
     range = 10,
     tool_capabilities = {
@@ -41,5 +57,36 @@ minetest.register_tool("fl_materials:meteorite_axe",{
             choppy = tool_stats
         },
         damage_groups = {fleshy=1}
+    }
+})
+
+---Recipes
+
+local v = "fl_materials:meteorite_ingot"
+local i = "default:stick"
+
+minetest.register_craft({
+
+    type = "shapeless",
+    output = "fl_materials:meteorite_ingot",
+    recipe = {"default:gold_ingot","default:gold_ingot","default:gold_ingot","default:gold_ingot",
+    "fl_materials:meteorite_fragment","fl_materials:meteorite_fragment","fl_materials:meteorite_fragment","fl_materials:meteorite_fragment"}
+    })
+
+minetest.register_craft({
+    output = "fl_materials:meteorite_pick",
+    recipe = {
+              {v,v,v},
+              {"",i,""},
+              {"",i,""}
+    }
+})
+
+minetest.register_craft({
+    output = "fl_materials:meteorite_axe",
+    recipe = {
+              {v,v,""},
+              {v,i,""},
+              {"",i,""}
     }
 })
--- mods/fl_materials/nodes.lua
+++ mods/fl_materials/nodes.lua
@@ -1,12 +1,11 @@
-minetest.register_node("fl_materials:stone_meteorite",{
 
-    description = "meteorite ore",
-    tiles = {"default_stone.png^meteorite.png"},
-    groups = {cracky = 1},
-    drop = "fl_materials:meteorite_fragment",
-    sounds = default.node_sound_stone_defaults()
 
-})
+minetest.clear_craft({type = "cooking", recipe ="default:iron_lump"})
+minetest.clear_craft({type = "cooking", recipe ="default:copper_lump"})
+minetest.clear_craft({type = "cooking", recipe ="default:gold_lump"})
+minetest.clear_craft({type = "cooking", recipe ="moreores:silver_lump"})
+
+
 
 minetest.override_item("default:wood",{
 
@@ -37,6 +36,75 @@ minetest.override_item("default:acacia_wood",{
     tiles = {{name = "acacia_wood_grid.png", align_style = "world", scale = 2}},
     iventory_image = '{inventorycube{acacia_wood_grid.png&[sheet:2x2:1,1{acacia_wood_grid.png&[sheet:2x2:1,1{acacia_wood_grid.png&[sheet:2x2:1,1',
 })
+
+minetest.register_node("fl_materials:stone_meteorite",{
+
+    description = "meteorite ore",
+    tiles = {"default_stone.png^meteorite.png"},
+    groups = {cracky = 1},
+    drop = "fl_materials:meteorite_fragment",
+    sounds = default.node_sound_stone_defaults()
+
+})
+
+minetest.register_node("fl_materials:copper_brute",{
+
+    description = "copper brute block",
+    tiles = {"copper_brute.png"},
+    groups = {cracky = 1},
+    sounds = default.node_sound_stone_defaults()
+
+})
+
+minetest.register_node("fl_materials:iron_brute",{
+
+    description = "Iron brute block",
+    tiles = {"iron_brute.png"},
+    groups = {cracky = 1},
+    sounds = default.node_sound_stone_defaults()
+})
+
+minetest.register_node("fl_materials:silver_brute",{
+
+    description = "Silver brute block",
+    tiles = {"silver_brute.png"},
+    groups = {cracky = 1},
+    sounds = default.node_sound_stone_defaults()
+})
+
+minetest.register_node("fl_materials:gold_brute",{
+
+    description = "Gold brute block",
+    tiles = {"gold_brute.png"},
+    groups = {cracky = 1},
+    sounds = default.node_sound_stone_defaults()
+})
+
+local recipes = {"default:copper_lump","default:iron_lump","default:gold_lump","moreores:silver_lump"}
+local material = {"fl_materials:copper_brute","fl_materials:iron_brute","fl_materials:gold_brute","fl_materials:silver_brute"}
+local ingots = {"default:copper_ingot","default:steel_ingot","default:gold_ingot","moreores:silver_ingot"}
+local i = 1
+
+for recipes, v in pairs(recipes) do 
+    minetest.register_craft({
+         type = "shapeless",
+         output = material[i],
+         recipe = {v,v,v,
+                   v,v,v,
+                   v,v,v}
+    })
+    minetest.register_craft({
+        type = "cooking",
+        output = ingots[i],
+        recipe = material[i],
+        cooktime = 20
+    })
+    i = i + 1
+end
+
+
+
+
 
 
 
