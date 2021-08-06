abc8b332c975791bfcdc085b25cdb40ef92b7591
WilLiam12 <65061739+WilLiam12-teddy@users.noreply.github.com>
2021-08-05 21:22:31 -0300

    ranks

---
+++ mods/ranks/ranks.lua
@@ -0,0 +1,16 @@
+-- ranks/ranks.lua
+
+ranks.register("admin", {
+	prefix = "Admin",
+	colour = {a = 255, r = 230, g = 33, b = 23},
+})
+
+ranks.register("dev", {
+	prefix = "Dev",
+	colour = {a = 255, r = 255, g = 83, b = 37},
+})
+
+ranks.register("gamer", {
+	prefix = "Gamer",
+	colour = {a = 255, r = 255, g = 132, b = 0},
+})
